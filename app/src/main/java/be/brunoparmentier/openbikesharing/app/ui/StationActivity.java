180441ff0450bd56f424467239106ca400c1bb21
bparmentier <dev@brunoparmentier.be>
2015-01-01 18:31:29 +0100

    Update copyright year :fireworks:

--- app/src/main/java/be/brunoparmentier/openbikesharing/app/ui/StationActivity.java
+++ app/src/main/java/be/brunoparmentier/openbikesharing/app/ui/StationActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Bruno Parmentier. This file is part of OpenBikeSharing.
+ * Copyright (c) 2014-2015 Bruno Parmentier. This file is part of OpenBikeSharing.
  *
  * OpenBikeSharing is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
